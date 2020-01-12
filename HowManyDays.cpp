8c1345745e3bbd866250d3fd0992f112643c04e5
IMALB <326885225@qq.com>
2020-01-12 21:41:57 +0800

    第一次修改

--- HowManyDays.cpp
+++ HowManyDays.cpp
@@ -6,14 +6,18 @@ using namespace std;
 
 array<int, 13> mon_days = {-1,31,28,31,30,31,30,31,31,30,31,30,31};
 
-struct Date {
+struct Date 
+{
     int year, mon, day;
 };
 
 bool isleap(int);
 bool isOK(int);
+bool isLegal(const Date &,const Date &);
 int cal_days(const Date &,const Date &);
 void print(const Date &,const Date &, int &);
+void getBeginTime(Date &); 
+void getNowTime(Date &, tm *);
 
 int main()
 {
@@ -21,54 +25,95 @@ int main()
     time_t now = time(0);
     tm *ltm = localtime(&now);
 
-    // 开始的日期,年，月，日
-    btm.year = 2019, btm.mon = 10, btm.day = 16;
-    // 现在的日期
-    ntm.year = 1900 + ltm->tm_year;
-    ntm.mon = 1 + ltm->tm_mon;
-    ntm.day = ltm->tm_mday;
+    getBeginTime(btm);
+    getNowTime(ntm, ltm);
 
     int ans = cal_days(btm, ntm);
     print(btm, ntm, ans);
     return 0;
 }
+// 获取开始的时间
+void getBeginTime(Date & btm)
+{
+    btm.year = 2019, btm.mon = 10, btm.day = 16;
+}
+// 获取结束的时间
+void getNowTime(Date & ntm, tm *ltm)
+{
+    ntm.year = 1900 + ltm->tm_year;
+    ntm.mon = 1 + ltm->tm_mon;
+    ntm.day = ltm->tm_mday;
+}
 // 计算天数的逻辑，先计算完整的年,完整的月
 int cal_days(const Date & btm,const Date & ntm)
 {
     int ans = 0;
-    for (int i = btm.year+1; i < ntm.year; i++) {
-        if (isleap(i)) ans += 366;
-        else           ans += 365;
+    if(btm.year == ntm.year && btm.mon == ntm.mon) {
+        ans = ntm.day - btm.day;
     }
-    if (isleap(btm.year))  mon_days[2] = 29;
-    else                   mon_days[2] = 28;
-    if (btm.year != ntm.year) 
-        for (int i = btm.mon+1; i <= 12; i++)
-            ans += mon_days[i];
-    else for (int i = btm.mon+1; i < ntm.mon; i++)
+    else {
+        for (int i = btm.year+1; i < ntm.year; i++) {
+            if (isleap(i)) ans += 366;
+            else           ans += 365;
+        }
+        if (isleap(btm.year))  mon_days[2] = 29;
+        else                   mon_days[2] = 28;
+        if (btm.year != ntm.year) 
+            for (int i = btm.mon+1; i <= 12; i++)
                 ans += mon_days[i];
-    ans += mon_days[btm.mon] - btm.day + 1;
-    
-    if (isleap(ntm.year)) mon_days[2] = 29;
-    else                  mon_days[2] = 28;
-    if (btm.year != ntm.year) 
-        for (int i = 1; i < ntm.mon; i++)
-            ans += mon_days[i];
-    ans += ntm.day;
-
+        else for (int i = btm.mon+1; i < ntm.mon; i++)
+                    ans += mon_days[i];
+        ans += mon_days[btm.mon] - btm.day + 1;
+        
+        if (isleap(ntm.year)) mon_days[2] = 29;
+        else                  mon_days[2] = 28;
+        if (btm.year != ntm.year) 
+            for (int i = 1; i < ntm.mon; i++)
+                ans += mon_days[i];
+        ans += ntm.day;
+    }
     return ans;
 }
-
+// 判断是否是闰年
 bool isleap(int n)
 {
     if (n%400==0 || (n%4==0&&n%100))
         return true;
     return false;
 }
-
+// 打印结果
 void print(const Date & btm,const Date & ntm, int & ans)
 {
-    cout << "begin time: " << btm.year << "." << btm.mon << "." << btm.day << endl;
-    cout << "now time: " << ntm.year << "." << ntm.mon << "." << ntm.day << endl;
-    cout << "how many days: " << ans << endl;
+    if (isLegal(btm, ntm)) {
+        cout << "begin time: " << btm.year << "." << btm.mon << "." << btm.day << endl;
+        cout << "now time: " << ntm.year << "." << ntm.mon << "." << ntm.day << endl;
+        cout << "how many days: " << ans << endl;   
+    }
+    else
+        cout << "the date is error!" << endl;
+}
+// 判断两个日期是否合法
+bool isLegal(const Date & btm,  const Date & ntm)
+{
+    if (btm.mon<1 || btm.mon>12 || ntm.mon<1 || ntm.mon>12)
+        return false;
+    else {
+        if (btm.mon == 2)
+            mon_days[2] = isleap(btm.year) ? 29 : 28;
+        if (btm.day<1 || btm.day>mon_days[btm.year] )
+            return false;
+
+        if (ntm.mon == 2)
+            mon_days[2] = isleap(ntm.year) ? 29 : 28;    
+        if (ntm.day<1 || ntm.day>mon_days[ntm.year])
+            return false;
+    }
+
+    if (btm.year < ntm.year)
+        return true;
+    else if(btm.year == ntm.year && btm.mon < ntm.mon)
+        return true;
+    else if(btm.year == ntm.year && btm.mon == ntm.mon && btm.day <= ntm.day)
+        return true;
+    return false;
 }
